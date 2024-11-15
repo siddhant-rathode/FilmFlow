3464a83849e24462b4dae726f7d5de8b4785f1f8
Siddhant Rathode <siddhantrathode@gmail.com>
2024-11-15 23:30:23 +0530

    boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='bg-[#1F1E24] w-screen h-screen flex'>
+      <Routes>
+        <Route  path="/" element={<Home />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    document.title ="FilmFlow | Home"
+  return <>
+  <div className='w-[20%] h-full bg-red-100'></div>
+  <div className='w-[80%] h-full bg-green-100'></div>
+  </>
+}
+
+export default Home
